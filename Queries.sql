a1438e2fd4f543b3900d12d320b0f2c1b893b161
Wesley Jacoby <wesleyjacoby@gmail.com>
2020-07-11 07:47:42 +0200

    Refactor code

--- Queries.sql
+++ Queries.sql
@@ -1,8 +1,8 @@
 /* Query 1: Who are the top 5 customers and the revenue they brought in (Incl. late fees)? */
 
 SELECT 	c.first_name || ' ' || c.last_name AS full_name,
-				SUM(p.amount) AS total_amt_paid,
-				COUNT(p.rental_id) AS num_movies_rented
+		SUM(p.amount) AS total_amt_paid,
+		COUNT(p.rental_id) AS num_movies_rented
 FROM customer c
 JOIN payment p
 ON c.customer_id = p.customer_id
@@ -14,7 +14,7 @@ LIMIT 5;
 /* Query 2:  What are the most popular movie genres in terms of revenue? */
 
 SELECT 	c.name AS genre,
-				SUM(f.rental_rate) AS revenue
+		SUM(f.rental_rate) AS revenue
 FROM category c
 JOIN film_category fc
 ON c.category_id = fc.category_id
@@ -33,9 +33,9 @@ LIMIT 5;
 
 WITH table1 AS (
 	SELECT 	c.customer_id,
-					c.first_name || ' ' || c.last_name AS full_name,
-					SUM(p.amount) AS total_amt_paid,
-					COUNT(*) AS num_movies_rented
+			c.first_name || ' ' || c.last_name AS full_name,
+			SUM(p.amount) AS total_amt_paid,
+			COUNT(*) AS num_movies_rented
 	FROM customer c
 	JOIN payment p
 	ON c.customer_id = p.customer_id
@@ -44,8 +44,8 @@ WITH table1 AS (
 	LIMIT 5)
 
 SELECT 	full_name,
-				c.name AS genre,
-				DENSE_RANK() OVER (PARTITION BY full_name ORDER BY COUNT(p.rental_id) DESC) AS genre_rank
+		c.name AS genre,
+		DENSE_RANK() OVER (PARTITION BY full_name ORDER BY COUNT(p.rental_id) DESC) AS genre_rank
 FROM table1
 JOIN payment p
 ON table1.customer_id = p.customer_id
@@ -67,8 +67,8 @@ ORDER BY 1, 3;
 
 WITH t1 AS (
 	SELECT 	c.first_name || ' ' || c.last_name AS full_name,
-					COUNT(*) AS num_dvds_rented,
-					SUM(p.amount) AS all_payments
+			COUNT(*) AS num_dvds_rented,
+			SUM(p.amount) AS all_payments
 	FROM customer c
 	JOIN payment p
 	ON c.customer_id = p.customer_id
@@ -76,7 +76,7 @@ WITH t1 AS (
 	ORDER BY 3 DESC)
 
 SELECT 	full_name,
-				ROUND((all_payments/num_dvds_rented), 2) AS average_payment
+		ROUND((all_payments/num_dvds_rented), 2) AS average_payment
 FROM t1
 GROUP BY 1, 2
 ORDER BY 2 DESC;
